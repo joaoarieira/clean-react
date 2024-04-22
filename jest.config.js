66c5b79b8211305a91cedbbe374da1af838a6d1a
Joao Azevedo <joaovarieira@gmail.com>
2024-04-22 11:10:30 -0300

    chore: fixing eslint

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 };
