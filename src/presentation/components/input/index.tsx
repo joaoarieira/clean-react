26dab2c2276bf9904ab6946409b0d1892dc65110
Joao Azevedo <joaovarieira@gmail.com>
2024-04-26 08:33:37 -0300

    feat: adding label to Input

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -2,26 +2,33 @@ import { useFormContext } from '@/presentation/contexts/form/use-form-context';
 import Styles from './styles.module.scss';
 import { useMemo } from 'react';
 
-type Props = React.DetailedHTMLProps<
+type Props = {
+  name: string;
+  label?: React.ReactNode;
+} & React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-export function Input({ name, ...props }: Props) {
+export function Input({ name, id, label, ...props }: Props) {
   const { errors } = useFormContext();
+  const inputId = id ?? `${name}-input`;
 
   const isErroed = useMemo(
     () => name != null && errors[name] != null,
     [errors, name],
   );
 
-  const errorMessage = isErroed ? errors[name!].message : '';
+  const errorMessage = isErroed ? errors[name].message : '';
 
   const statusIndicator = '🔴';
 
   return (
     <div className={Styles['input-wrapper']}>
-      <input {...props} />
+      {label != null && <label htmlFor={inputId}>{label}</label>}
+
+      <input id={inputId} name={name} {...props} />
+
       <span
         className={Styles.status}
         title={errorMessage}
