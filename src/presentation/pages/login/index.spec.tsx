81daeafe65a4b50157085e741f0d4044071b3266
Joao Azevedo <joaovarieira@gmail.com>
2024-04-26 07:56:44 -0300

    refactor: move Login SUT creation to a factory method

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -1,33 +1,42 @@
 import { Login } from '@/presentation/pages/login';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 
-// const user = userEvent.setup();
+const makeSut = () => {
+  const renderResult = render(<Login />);
+  const user = userEvent.setup();
+
+  return {
+    sut: screen,
+    user,
+    renderResult,
+  };
+};
 
 describe('Login', () => {
   test('should not show error message when mounting', () => {
-    render(<Login />);
-    const errorMessageComponent = screen.queryByTestId('form-status-error');
+    const { sut } = makeSut();
+    const errorMessageComponent = sut.queryByTestId('form-status-error');
     expect(errorMessageComponent).toBeNull();
   });
 
   test('should not show loading spinner when mounting', () => {
-    render(<Login />);
-    const loadingSpinnerComponent = screen.queryByTestId('form-status-loading');
+    const { sut } = makeSut();
+    const loadingSpinnerComponent = sut.queryByTestId('form-status-loading');
     expect(loadingSpinnerComponent).toBeNull();
   });
 
   test('should have submit button disabled when mounting', () => {
-    render(<Login />);
-    const submitButton = screen.getByRole<HTMLButtonElement>('button', {
+    const { sut } = makeSut();
+    const submitButton = sut.getByRole<HTMLButtonElement>('button', {
       name: 'Entrar',
     });
     expect(submitButton.disabled).toBe(true);
   });
 
   test('should have its email field status indicator advising that it is required', () => {
-    render(<Login />);
-    const statusComponent = screen.getByTestId<HTMLSpanElement>('email-status');
+    const { sut } = makeSut();
+    const statusComponent = sut.getByTestId<HTMLSpanElement>('email-status');
     expect(statusComponent.textContent).toBe('🔴');
     expect(statusComponent.title).toBe('campo obrigatório');
   });
