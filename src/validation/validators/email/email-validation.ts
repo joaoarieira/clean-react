c67ec2d76d76e75afa09c22981087172126f854c
Joao Azevedo <joaovarieira@gmail.com>
2024-05-02 20:32:32 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,7 +5,8 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error | undefined {
-    console.log(value);
-    return new InvalidFieldError();
+    const emailRegExp =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return emailRegExp.test(value) ? undefined : new InvalidFieldError();
   }
 }
