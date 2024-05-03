ebeedf635007f3e6a095b5402fa0a70ac152aa58
Joao Azevedo <joaovarieira@gmail.com>
2024-05-03 09:32:25 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,14 +2,32 @@ import { InvalidFieldError } from '@/validation/errors';
 import { faker } from '@faker-js/faker';
 import { MinLengthValidation } from './min-length-validation';
 
+function makeSut() {
+  const minLength = faker.number.int({ max: 20 });
+  const sut = new MinLengthValidation(faker.lorem.word(), minLength);
+  return { sut, minLength };
+}
+
 describe('MinLengthValidation', () => {
   test('should return error if value is invalid', () => {
-    const minLength = faker.number.int();
-    const wordLength = faker.number.int({ min: 0, max: minLength - 1 });
-    const sut = new MinLengthValidation(faker.lorem.word(), minLength);
+    const { sut, minLength } = makeSut();
+    const wordShorterThanMinLength = faker.string.alphanumeric({
+      length: { min: 0, max: minLength - 1 },
+    });
 
-    const error = sut.validate(faker.lorem.word({ length: wordLength }));
+    const error = sut.validate(wordShorterThanMinLength);
 
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test('should return falsy if value is valid', () => {
+    const { sut, minLength } = makeSut();
+    const wordGreaterThanMinLength = faker.string.alphanumeric({
+      length: { min: minLength, max: 50 },
+    });
+
+    const error = sut.validate(wordGreaterThanMinLength);
+
+    expect(error).toBeFalsy();
   });
 });
